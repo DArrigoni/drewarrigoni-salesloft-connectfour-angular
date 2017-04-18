93f1d8c8c4e206a4a31da249ffa7cb3be1d5fbde
Drew Arrigoni <arrigoni.andrew@gmail.com>
2017-04-18 12:20:40 -0400

    #143803421 - Refactor win condition code

--- src/app/components/game-board/game-board.component.ts
+++ src/app/components/game-board/game-board.component.ts
@@ -9,19 +9,33 @@ export class GameBoardComponent {
   static readonly PLAYER1 = 1;
   static readonly PLAYER2 = 2;
 
-  //TODO: Make the width/length configurable
-  gameState = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0]
-  ];
+  gameState: Number[][];
+  activePlayer: Number;
+  won: Boolean;
 
-  activePlayer = GameBoardComponent.PLAYER1;
-  won = false;
+  minWinLength: Number;
+  width: Number;
+  height: Number;
+
+  //TODO: Make the width/length configurable
+  constructor() {
+    this.gameState = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0]
+    ];
+
+    this.activePlayer = GameBoardComponent.PLAYER1;
+    this.won = false;
+
+    this.minWinLength = 4;
+    this.width = this.gameState.length;
+    this.height = this.gameState[0].length;
+  }
 
   play(column) {
     //if(won) { return; } //Still trying to play while the game is won? Silly.
@@ -29,91 +43,73 @@ export class GameBoardComponent {
     let openIndex = this.gameState[column].indexOf(0);
     this.gameState[column][openIndex] = this.activePlayer;
 
-    //Could make these magic numbers but this is just as clear
-    let width = this.gameState.length;
-    let height = this.gameState[0].length;
-    //
-    //TODO: Make this a configurable number
-    let minWinLength = 4;
+    this.won = this.checkWin();
+  }
 
-    let playerWinString = Array(minWinLength).fill(this.activePlayer).join('');
-    console.log(playerWinString);
+  private checkWin() {
+    let playerWinString = Array(this.minWinLength).fill(this.activePlayer).join('');
 
-    //Column check
-    for( let column of this.gameState ) {
-      let columnString = column.join('');
-      if(columnString.indexOf(playerWinString) >= 0) {
-        this.won = true;
-        break;
-      }
-    }
-
-    //Row check
-    for( let rowIndex = 0; rowIndex < height; rowIndex++ ) {
-      let rowString = '';
-      for ( let column of this.gameState ) {
-        rowString = rowString.concat(String(column[rowIndex]));
-      }
-
-      if(rowString.indexOf(playerWinString) >= 0) {
-        this.won = true;
-        break;
-      }
-    }
-
-    //Left to right diagonal check - AKA sum the indexes and key by the sum
-    let ltrDiagonalStrings = {};
-
-    //Generate diagonal strings
-    this.gameState.forEach((column, columnIndex)=> {
-      column.forEach((cellValue, rowIndex)=> {
-        let cell = String(cellValue);
-        let indexSum = columnIndex + rowIndex;
-        let currentValue = ltrDiagonalStrings[indexSum]
-        if(currentValue) {
-          ltrDiagonalStrings[indexSum] = currentValue.concat(cell);
-        } else {
-          ltrDiagonalStrings[indexSum] = cell;
-        }
-      })
-    });
-
-    //Check Diagonal strings
-    for( let key in ltrDiagonalStrings ) {
-      let diagonalString = ltrDiagonalStrings[key];
-      console.log(diagonalString)
-      if(diagonalString.indexOf(playerWinString) >= 0) {
-        this.won = true;
-        break;
-      }
-    }
-    //
-    //Left to right diagonal check - AKA sum the indexes and key by the sum
-    let rtlDiagonalStrings = {};
-
-    //Generate diagonal strings
     let reversedGameState = this.gameState.slice().reverse();
-    reversedGameState.forEach((column, columnIndex)=> {
+
+    let columnStrings = this.generateColumnStrings(this.gameState);
+    let rowStrings = this.generateRowStrings(this.gameState);
+    let ltrDiagonalStrings = this.generateDiagonalStrings(this.gameState);
+    let rtlDiagonalStrings = this.generateDiagonalStrings(reversedGameState)
+
+    let possibleWinStrings = columnStrings
+      .concat(rowStrings)
+      .concat(ltrDiagonalStrings)
+      .concat(rtlDiagonalStrings);
+
+    if(this.checkForWinString(possibleWinStrings)) { return true; }
+  }
+
+  private checkForWinString(checkStrings) {
+    let playerWinString = Array(4).fill(this.activePlayer).join('');
+
+    return checkStrings.some((checkString)=> {
+      return checkString.indexOf(playerWinString) >= 0
+    });
+  }
+
+  private generateColumnStrings(gameState) {
+    return gameState.map((column)=> {
+      return column.join('');
+    })
+  }
+
+  private generateRowStrings(gameState) {
+    let rowStrings = [];
+    for( let rowIndex = 0; rowIndex < this.height; rowIndex++ ) {
+      let rowString = gameState.map((column)=> { return column[rowIndex]; }).join('');
+
+      rowStrings.push(rowString);
+    }
+    return rowStrings;
+  }
+
+  private generateDiagonalStrings(gameState) {
+    let diagonalStringsByIndexSum = {};
+
+    gameState.forEach((column, columnIndex)=> {
       column.forEach((cellValue, rowIndex)=> {
         let cell = String(cellValue);
         let indexSum = columnIndex + rowIndex;
-        let currentValue = rtlDiagonalStrings[indexSum]
+        let currentValue = diagonalStringsByIndexSum[indexSum]
+
         if(currentValue) {
-          rtlDiagonalStrings[indexSum] = currentValue.concat(cell);
+          diagonalStringsByIndexSum[indexSum] = currentValue.concat(cell);
         } else {
-          rtlDiagonalStrings[indexSum] = cell;
+          diagonalStringsByIndexSum[indexSum] = cell;
         }
       })
     });
 
-    //Check Diagonal strings
-    for( let key in rtlDiagonalStrings ) {
-      let diagonalString = rtlDiagonalStrings[key];
-      console.log(diagonalString)
-      if(diagonalString.indexOf(playerWinString) >= 0) {
-        this.won = true;
-        break;
-      }
+    let diagonalStrings = [];
+    for( let key in diagonalStringsByIndexSum ) {
+      diagonalStrings.push(diagonalStringsByIndexSum[key]);
     }
+
+    return diagonalStrings;
   }
 }
